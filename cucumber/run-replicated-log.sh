c2a6e9f7909913b4d9e94ff01017e0971a99418e
Yuliia Shandra <yuliia.shandra@gmail.com>
2021-12-08 12:05:55 +0200

    Make docker compose runnable

--- cucumber/run-replicated-log.sh
+++ cucumber/run-replicated-log.sh
@@ -1,7 +1,2 @@
-cd secondary
-docker build --rm -t secondary-node:1.1 .
-cd -
-cd master
-docker build --rm -t master-node:1.0 .
-cd -
-docker-compose up -d
+docker-compose -f docker-compose.yml up
+# docker-compose -f docker-compose.yml up --build
